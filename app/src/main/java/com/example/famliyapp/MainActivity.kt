e87423e5292718666eb7ac32bb01251f151050c7
SinghShweta1517 <shweta8462603@gmail.com>
2023-12-29 18:55:38 +0530

    init project

---
+++ app/src/main/java/com/example/famliyapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.famliyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+         val bottomBar = findViewById<BottomNavigationView>(R.id.bottom_bar)
+        bottomBar.setOnItemSelectedListener {menuItem ->
+            if(menuItem.itemId==R.id.navGuard){
+                inflateFragment(GuardFragment.newInstance())
+            }else if(menuItem.itemId == R.id.navHome){
+                inflateFragment(HomeFragment.newInstance())
+            }
+
+            true
+        }
+
+        }
+
+
+    private fun inflateFragment(newInstance: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,newInstance)
+        transaction.commit()
+
+    }
+}
+
+
+
+
